510106ceb56ccbc0e13856fbd9bbf9d7ec4bd4df
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-12-29 17:41:53 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+class Command {
+	private pattern = /CMD +((([\w-.]+\/[\w-.]+)?#\d+) *((, *)? *and +|, *)?)+/i
+
+	constructor(
+		private cmd: RegExp,
+		private callback: (context: any, issues: RegExpMatchArray) => void
+	) {}
+
+	get command() {
+		const raw = this.pattern.source
+		// Warning: We ignore this.cmd flags
+		return new RegExp(raw.replace(/CMD/, this.cmd.source), this.pattern.flags)
+	}
+
+	issues(str: string) {
+		return str.match(/(?:[\w-.]+\/[\w-.]+)?#\d+/g)
+	}
+
+	listener(context: any) {
+		const match = (context.payload.comment.body as string).match(this.command)
+		if (match) {
+			this.callback(context, this.issues(match[0]) as RegExpMatchArray)
+		}
+	}
+}
+
+/**
+ * A Probot extension to make it easier to working with issue commands.
+ *
+ * @example
+ *
+ * // Type `closes #1, and owner/repo#2`
+ * commands(robot, /closes/, (context, issues) => {
+ *   console.log(issues)
+ *   // ['#1','owner/repo#2', ..etc]
+ * });
+ */
+export default (
+	robot: any,
+	name: RegExp,
+	callback: (context: any, issues: RegExpMatchArray) => void
+) => {
+	const command = new Command(name, callback)
+	robot.on('issue_comment.created', command.listener.bind(command))
+}
---
+++ test.ts
@@ -0,0 +1,58 @@
+// Native
+import { EventEmitter } from 'events'
+
+// Ours
+import commands from '.'
+
+describe('commands', () => {
+	let callback: any
+	let robot: any
+
+	function payload(text: string) {
+		return { payload: { comment: { body: text } } }
+	}
+
+	beforeEach(() => {
+		callback = jest.fn()
+		robot = new EventEmitter()
+		commands(robot, /foo(ing)?/, callback)
+	})
+
+	it('invokes callback and passes issues list', () => {
+		robot.emit('issue_comment.created', payload('hello world\n\nfoo #1'))
+
+		expect(callback).toHaveBeenCalled()
+		expect(callback.mock.calls[0][1]).toEqual(['#1'])
+	})
+
+	it('supports case-incensitive matching', () => {
+		robot.emit('issue_comment.created', payload('FoO #1'))
+
+		expect(callback).toHaveBeenCalled()
+		expect(callback.mock.calls[0][1]).toEqual(['#1'])
+	})
+
+	it('supports complex issue patterns', () => {
+		robot.emit('issue_comment.created', payload('fooing #1, #2 ,and a/b#3'))
+
+		expect(callback).toHaveBeenCalled()
+		expect(callback.mock.calls[0][1]).toEqual(['#1', '#2', 'a/b#3'])
+	})
+
+	it('does not call callback for other commands', () => {
+		robot.emit(
+			'issue_comment.created',
+			payload('hello world\n\n/nope nothing to see')
+		)
+		expect(callback).not.toHaveBeenCalled()
+	})
+
+	it('invokes command on issue edit', () => {
+		const event = payload('hello world\n\n/foo bar')
+		robot.emit('issue_comment', event)
+		robot.emit('issue_comment.updated', event)
+		robot.emit('issue_comment.deleted', event)
+
+		expect(callback).not.toHaveBeenCalled()
+	})
+})
